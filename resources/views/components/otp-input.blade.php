eb8211c2c05da97eba9da8dfd56a40c120da4049
SALAHADEEN_DEV <78902162+SALAHADEEN-DEV@users.noreply.github.com>
2026-03-06 00:18:00 +0300

    a

--- resources/views/components/otp-input.blade.php
+++ resources/views/components/otp-input.blade.php
@@ -21,7 +21,7 @@
 
 <x-dynamic-component
     :component="$getFieldWrapperView()"
-    :id="$getId()"
+    :id="$id"
     :label="$getLabel()"
     :label-sr-only="$isLabelHidden()"
     :helper-text="$getHelperText()"
@@ -29,13 +29,13 @@
     :hint-actions="$getHintActions()"
     :hint-icon="$getHintIcon()"
     :required="$isRequired()"
-    :state-path="$getStatePath()"
+    :state-path="$statePath"
 >
     <div x-data="{
-    	    state: $wire.$entangle('{{ $getStatePath() }}'),
-    	    length: {{$numberInput}},
-    	    autoFocus: '{{$isAutofocused}}',
-    	    type: '{{$inputType}}',
+            state: $wire.$entangle('{{ $statePath }}'),
+            length: {{ $numberInput }},
+            autoFocus: {{ $isAutofocused }},
+            type: '{{ $inputType }}',
             init: function(){
                 if (this.autoFocus){
                     this.$refs[1].focus();
@@ -48,17 +48,17 @@
                 }
 
                 this.state = Array.from(Array(this.length), (element, i) => {
-                    const el = this.$refs[(i + 1)];
+                    const el = this.$refs[i + 1];
                     return el.value ? el.value : '';
                 }).join('');
 
-
-                if (i < this.length) {
-                    this.$refs[i+1].focus();
-                    this.$refs[i+1].select();
+                if (i < this.length - 1) {
+                    this.$refs[i + 1].focus();
+                    this.$refs[i + 1].select();
                 }
-                if(i == this.length){
-                    @this.set('{{ $getStatePath() }}', this.state)
+
+                if(i == this.length - 1){
+                    @this.set('{{ $statePath }}', this.state);
                 }
             },
 
@@ -68,8 +68,8 @@
                 const inputs = Array.from(Array(this.length));
 
                 inputs.forEach((element, i) => {
-                    this.$refs[(i+1)].focus();
-                    this.$refs[(i+1)].value = paste[i] || '';
+                    this.$refs[i + 1].focus();
+                    this.$refs[i + 1].value = paste[i] || '';
                 });
             },
 
@@ -85,7 +85,6 @@
         <div class="flex justify-between gap-6 fi-otp-input-container" dir="{{ $isRtl ? 'rtl' : 'ltr' }}">
 
             @foreach(range(1, $numberInput) as $column)
-
                 <x-filament::input.wrapper
                     :disabled="$isDisabled"
                     :inline-prefix="$isPrefixInline"
@@ -98,25 +97,21 @@
                     :suffix-actions="$suffixActions"
                     :suffix-icon="$suffixIcon"
                     :suffix-icon-color="$getSuffixIconColor()"
-                    :valid="! $errors->has($statePath)"
-                    :attributes="
-                        \Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())
-                        ->class(['fi-fo-text-input overflow-hidden'])
-                    "
+                    :valid="!$errors->has($statePath)"
+                    :attributes="Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())->class(['fi-fo-text-input overflow-hidden'])"
                 >
                     <input
-                        {{$isDisabled ? 'disabled' : ''}}
-                        type="{{$inputType}}"
+                        {{ $isDisabled ? 'disabled' : '' }}
+                        type="{{ $inputType }}"
                         maxlength="1"
-                        x-ref="{{$column}}"
+                        x-ref="{{ $column }}"
                         required
-                        autocomplete="{{$autocomplete}}"
+                        autocomplete="{{ $autocomplete }}"
                         class="fi-input fi-otp-input block w-full border-none py-1.5 text-base text-gray-950 transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6 bg-white/0 ps-3 pe-3 text-center"
-                        x-on:input="handleInput($event, {{$column}})"
+                        x-on:input="handleInput($event, {{ $column }})"
                         x-on:paste="handlePaste($event)"
                         x-on:keydown.backspace="handleBackspace($event)"
                     />
-
                 </x-filament::input.wrapper>
             @endforeach
 
@@ -137,6 +132,6 @@
         -webkit-appearance: none;
         -moz-appearance: none;
         appearance: none;
-        margin: 0
+        margin: 0;
     }
 </style>
--- src/Components/OtpInput.php
+++ src/Components/OtpInput.php
@@ -1,15 +1,16 @@
 <?php
+
 namespace HasanAhani\FilamentOtpInput\Components;
 
-use _PHPStan_11268e5ee\Nette\PhpGenerator\Closure;
-use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Field;
 use Filament\Notifications\Notification;
 use Filament\Support\Concerns\HasExtraAlpineAttributes;
 use Filament\Forms\Components\Concerns;
-use Filament\Forms\Components\Contracts;
+use Filament\Forms\Components\Contracts\CanBeLengthConstrained;
+use Filament\Forms\Components\Contracts\HasAffixActions;
+use Closure;
 
-class OtpInput extends Field implements Contracts\CanBeLengthConstrained, Contracts\HasAffixActions
+class OtpInput extends Field implements CanBeLengthConstrained, HasAffixActions
 {
     use Concerns\CanBeAutocapitalized;
     use Concerns\CanBeAutocompleted;
@@ -19,54 +20,72 @@ class OtpInput extends Field implements Contracts\CanBeLengthConstrained, Contra
     use Concerns\HasExtraInputAttributes;
     use HasExtraAlpineAttributes;
 
+    // Define the default view for the component
     protected string $view = 'filament-otp-input::components.otp-input';
 
-    protected int | \Closure | null $numberInput = 4;
+    // Default number of inputs in OTP field
+    protected int | Closure | null $numberInput = 4;
 
-    protected bool | \Closure | null $isRtl = false;
+    // Right-to-left (RTL) support flag
+    protected bool | Closure | null $isRtl = false;
 
-    protected string | \Closure | null $type = 'number';
+    // Default input type (number)
+    protected string | Closure | null $type = 'number';
 
-    public function numberInput(int | \Closure $number = 4):static
+    // Set the number of input fields for OTP
+    public function numberInput(int | Closure $number = 4): static
     {
         $this->numberInput = $number;
         return $this;
     }
 
-    public function getNumberInput():int
+    // Get the number of input fields for OTP
+    public function getNumberInput(): int
     {
         return $this->evaluate($this->numberInput);
     }
 
-
+    // Set the input type to password
     public function password(): static
     {
         $this->type = 'password';
-
         return $this;
     }
 
+    // Set the input type to text
     public function text(): static
     {
         $this->type = 'text';
-
         return $this;
     }
 
+    // Get the input type (text or password)
     public function getType(): string
     {
         return $this->evaluate($this->type);
     }
 
-    public function rtl(bool|\Closure $condition = false): static
+    // Enable or disable RTL (right-to-left) layout
+    public function rtl(bool | Closure $condition = false): static
     {
         $this->isRtl = $condition;
-
         return $this;
     }
 
+    // Get the direction for input fields (LTR or RTL)
     public function getInputsContainerDirection(): string
     {
         return $this->evaluate($this->isRtl);
+    }
+
+    // Action to trigger when user clicks "Clone"
+    public function actionClone($record)
+    {
+        // Clone the current record
+        $clonedRecord = $record->replicate();
+        $clonedRecord->save();
+
+        // Redirect to the edit page of the cloned record
+        return redirect()->route('filament.resources.' . strtolower(class_basename($record)) . '.edit', ['record' => $clonedRecord->id]);
     }
 }
